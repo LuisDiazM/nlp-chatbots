e852823100b4cef12cd8081fadce747130446b50
LuisDiazM <luismigueldiazmorales@gmail.com>
2022-08-14 16:17:50 -0500

    feat: restart the project

--- infraestructure/databases/mongo.py
+++
@@ -1,5 +0,0 @@
-
-
-class MongoImp:
-    def __init__(self) -> None:
-        print("mongo implementation constructor")
--- infraestructure/messaging/nats.py
+++
@@ -1,6 +0,0 @@
-
-class NatsImp:
-
-    def __init__(self) -> None:
-        print("nats implementation constructor")
-
