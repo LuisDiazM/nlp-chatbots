ed33af9f1a1b63f7099e9878e6ec6521499b6185
LuisDiazM <luismigueldiazmorales@gmail.com>
2022-08-14 17:34:49 -0500

    feat(backend): add api key

--- backend/go-core/app/router.go
+++ backend/go-core/app/router.go
@@ -1,6 +1,13 @@
 package app
 
-import "net/http"
+import (
+	"net/http"
+	"os"
+	"strings"
+
+	_ "github.com/LuisDiazM/goCore/docs"
+	httpSwagger "github.com/swaggo/http-swagger"
+)
 
 func enableCors(responseWriter *http.ResponseWriter) {
 	//Enable CORS
@@ -13,7 +20,7 @@ func enableCors(responseWriter *http.ResponseWriter) {
 
 func (app Application) Setup() {
 	app.Router.Use(app.route)
-
+	app.Router.PathPrefix("/swagger/").Handler(httpSwagger.WrapHandler)
 	goCore := app.Router.PathPrefix("/api/v1").Subrouter()
 	goCore.Path("/").HandlerFunc(app.Index).Methods(http.MethodGet, http.MethodOptions)
 }
@@ -22,6 +29,15 @@ func (app Application) route(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(responseWriter http.ResponseWriter, request *http.Request) {
 		enableCors(&responseWriter)
 		if (*request).Method == http.MethodOptions {
+			return
+		}
+		if strings.Contains(request.RequestURI, "/swagger") {
+			next.ServeHTTP(responseWriter, request)
+			return
+		}
+		apiKey := request.Header.Get("x-api-key")
+		if apiKey != os.Getenv("API_KEY") {
+			http.Error(responseWriter, "", http.StatusUnauthorized)
 			return
 		}
 		next.ServeHTTP(responseWriter, request)
