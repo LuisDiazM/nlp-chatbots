40668e5271794c1f6b161f97e81e37c80012868e
LuisDiazM <luismigueldiazmorales@gmail.com>
2023-10-16 21:46:10 -0500

    chore: update docker settings

--- frontend/chat-maker-ui/src/environments/environments.ts
+++ frontend/chat-maker-ui/src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   CLIENT_ID:
     "74994666463-ilp90899u78hq4dnuknaenct40fn8pa9.apps.googleusercontent.com",
-  BACKEND_URL: "http://localhost:8080"
+  BACKEND_URL: "http://127.0.0.1:8080"
 };
